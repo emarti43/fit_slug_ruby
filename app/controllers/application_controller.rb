1fed848df850c94df75f3aefd8c7c7ca117d5466
bastion6429 <emarti43@ucsc.edu>
2018-12-16 17:04:42 -0800

    api call works with NDB_API_KEY

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    response = RestClient::Request.execute(
-      method: :get,
-      url: 'https://api.nal.usda.gov/ndb/V2/reports?ndbno=01009&type=b&format=json&api_key=DEMO_KEY',
-    )
+    response = RestClient.get 'https://api.nal.usda.gov/ndb/V2/reports', {params: {ndbno:'01009', type: 'b', format: 'json', api_key: ENV["NDB_API_KEY"]}}
     render html: response
   end
 end
