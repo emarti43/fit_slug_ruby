34fbfa5bc999517f4d2f5980212ecf349bf77d90
bastion6429 <emarti43@ucsc.edu>
2018-12-19 15:40:09 -0800

    added exercises to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_19_224654) do
+ActiveRecord::Schema.define(version: 2018_12_19_233302) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.string "muscles"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "meals", force: :cascade do |t|
     t.string "name"
