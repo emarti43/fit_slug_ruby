7c97d01fcbd1fbb66206cb2532fca37632164a91
bastion6429 <emarti43@ucsc.edu>
2019-07-18 14:39:30 -0700

    sessions endpoint sets cookies for requests

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,31 +1,41 @@
 module Api
   class SessionsController < ApplicationController
+    protect_from_forgery with: :exception
     protect_from_forgery with: :null_session
-    def new
+    skip_before_action :verify_authenticity_token
+    def new_user
+      @user = User.new(user_params)
+      respond_to do |format|
+        if @user.save
+          session[:user_id] = {value: user.id, expires: 1.minutes.from_now}
+          render json: {}, status: :ok
+        else
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
     end
+
     def create
       user = User.find_by(email: params[:email].downcase)
       if user && user.authenticate(params[:password])
-        render json: {token: JsonWebToken.encode(user_id: user.id)}, status: :ok
+        session[:user_id] = {value: user.id, expires: 1.minutes.from_now}
+        render json: {'valid password': true}, status: :ok
       else
         render json: [], status: :bad_request, text: "invalid credentials"
       end
     end
 
     def validate
-      if request.headers['Authorization'].present?
-        decoded_token = JsonWebToken.decode(request.headers['Authorization'])
-        user = User.find(decoded_token[:user_id])
-        if user
-          render json: [], status: :ok
-        else
-          render json: [], status: bad_request, text: "invalid authentication token"
-        end
+      if !!session[:user_id]
+        render json: {'valid': true}, status: :ok
       else
-        render json: [], status: :bad_request, text: "No authenication token found"
+        render json: {'valid': false}, status: :bad_request, text: "invalid cookie"
       end
     end
-    def destroy
+
+    private
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation, :email)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,12 @@ Rails.application.routes.draw do
     resources :meals
     resources :exercise_records
     resources :meal_records
+        
     get '/login', to:'sessions#new'
-    post 'login',  to: 'sessions#create'
-    delete 'logout', to: 'sesssions#destroy'
-    get '/validate', to: 'sessions#validate'
+    post '/login',  to:'sessions#create'
+    delete '/logout', to:'sessions#destroy'
+    get '/validate', to:'sessions#validate'
+    get '/new_user', to:'sessions#new_user'
   end
   root 'application#hello'
 end
