4bd56cbbc0a19679982970b283e856b43765881d
bastion6429 <emarti43@ucsc.edu>
2019-08-09 16:34:36 -0700

    allowany origins

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module FitSlug
     # the framework and any gems in your application.
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:8080', 'localhost:3000'
+        origins '*'
         resource '*', headers: :any, methods: [:get, :post, :delete, :put, :options]
       end
     end
